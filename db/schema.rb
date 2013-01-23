55a461034b0531b8c9bb41cc232cb35d9184836d
Renaud Tircher <renaud.tircher@gmail.com>
2013-01-23 14:39:55 +0800

    Generating rapns migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130123063622) do
+ActiveRecord::Schema.define(:version => 20130123063904) do
 
   create_table "rapns_apps", :force => true do |t|
     t.string   "name",                       :null => false
