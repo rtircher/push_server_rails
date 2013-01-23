eef7c3ba279fb6b29ffa16d3dcabbe5756522c37
Renaud Tircher <renaud.tircher@gmail.com>
2013-01-23 14:38:44 +0800

    Creating rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PushServer::Application.config.secret_token = 'b1414a9fe218af8dd132db6a17db9748f71ec53d02687b0dcf60730b49e26b3456998565a7dfae759bd2e990004a39557c9a9e74f07f894de6a7fc16755306a1'
